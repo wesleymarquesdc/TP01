828f948a18e2fb27c6d78f2a45e821014699f881
Lucas Paulo <lucaspaulo1@hotmail.com>
2025-04-26 16:09:23 -0300

    cria pagina de cadastro de usuarios

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -3,10 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 // import App from './App.jsx'
 
-import Login from './LoginPage/Login.jsx'
+// import Login from './pages/LoginPage/Login.jsx'
+import RegisterUser from './pages/RegisterUserPage/RegisterUser.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Login />
+    <RegisterUser />
   </StrictMode>,
 )
