5fddb9907d02c99a306195e8e73adfabfe6010ad
Lucas Paulo <lucaspaulo1@hotmail.com>
2025-04-26 16:48:08 -0300

    criacao do feed de itens

---
+++ front-end/src/components/Feed/Feed.jsx
@@ -0,0 +1,48 @@
+import "../../pages/DashboardPage/Dashboard.css"
+
+
+import { useEffect, useState } from 'react';
+import FeedItem from '../FeedItem/FeedItem.jsx';
+import axios from "axios";
+
+const Feed = () => {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+  useEffect( () => {
+    // Substitua pelo back-end real:
+    axios.get(`${BASE_URL}/items`)
+      .then(res => {
+          setItems(res.data)
+          setIsLoading(false)
+      })
+      .catch(err => {
+        console.error('Erro ao carregar itens:', err)
+        setError(err)
+      })
+  }, []);
+
+  if (isLoading) return <div className="loading">Carregando...</div>;
+  if (error) return <div className="error">Erro: {error}</div>;
+  if (items.length === 0) return <div className="no-items">Nenhum item encontrado</div>;
+
+
+  return (
+    <>
+      {items == null ? 
+        <div className="feed-items">
+          {items.map(item => (
+            <FeedItem key={item.id} item={item} />
+          ))}
+        </div> : <div>Nao foi possivel carregar o feed</div>
+      
+      }
+    
+      </>
+  );
+};
+
+export default Feed;
